93cc1f42694722536e5d35dcb652832e8500c4fd
You Jhin <73587140+uJhin@users.noreply.github.com>
2021-01-12 02:54:52 +0900

    Upload Version 1.1.6.13
    Upload Version 1.1.6.13

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='upbit_client',
+    version='1.1.6.13',
+    packages=find_packages(),
+    install_requires=[
+        'bravado>=11.0.2',
+        'PyJWT>=2.0'
+    ],
+    extras_require={
+        'fido': [
+            'fido>=4.2.1'
+        ]
+    },
+    python_requires='>=3.8',
+    keywords=[
+        'Upbit',
+        'upbit',
+        'upbit-client',
+        'Upbit-Client',
+        'Upbit_client',
+        'upbit'
+    ],
+    url='https://github.com/uJhin/upbit-client',
+    download_url='https://github.com/uJhin/upbit-client/releases',
+    license='MIT License',
+    author='ujhin',
+    author_email='ujhin942@gmail.com',
+    description='Upbit OPEN API Client',
+    long_description=long_description
+)
